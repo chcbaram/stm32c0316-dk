3fadaaab499e46586c9505a0f80a041406333e68
Hancheol Cho <chcbaram@gmail.com>
2023-02-12 17:13:54 +0900

    초기 프로젝트 추가

---
+++ stm32c0_led/App/ap.c
@@ -0,0 +1,26 @@
+/*
+ * ap.c
+ *
+ *  Created on: Feb 12, 2023
+ *      Author: baram
+ */
+
+
+#include "ap.h"
+
+
+
+
+
+void apInit(void)
+{
+
+}
+
+void apMain(void)
+{
+  while(1)
+  {
+
+  }
+}
---
+++ stm32c0_led/App/ap.h
@@ -0,0 +1,17 @@
+/*
+ * ap.h
+ *
+ *  Created on: Feb 12, 2023
+ *      Author: baram
+ */
+
+#ifndef AP_H_
+#define AP_H_
+
+
+
+void apInit(void);
+void apMain(void);
+
+
+#endif /* AP_H_ */
